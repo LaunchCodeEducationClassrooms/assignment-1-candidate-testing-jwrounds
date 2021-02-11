b941a4499b88eaf855475d3e03144de3b51e96e9
jwrounds <jwrounds@usfca.edu>
2021-02-11 22:37:35 +0000

    Added question, grading, and display functionality to quiz app.

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,44 +3,79 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = '';
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let question = 'Who was the first American woman in space? ';
+let correctAnswer = 'Sally Ride';
+let candidateAnswer = '';
+let questions = ['Who was the first American woman in space?', 
+                 'True or false: 5000 meters = 5 kilometers.',
+                 '(5 + 3)/2 * 10 = ?',
+                 `Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2?`,
+                 'What is the minimum crew size for the International Space Station (ISS)?'];
+let correctAnswers = ['Sally Ride', 'True', '40', 'Trajectory', '3'];
+let candidateAnswers = [];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+  candidateName = input.question("What is your name?\n");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
+  for (let i = 0; i < questions.length; i++) {
+    candidateAnswer = input.question(`${questions[i]}\n`);
+    candidateAnswers.push(candidateAnswer);
+  }
 
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
-
-  let grade;
+  let grade = 0;
   
+  for (let i = 0; i < candidateAnswers.length; i++) {
+    if (candidateAnswers[i].toLowerCase() === correctAnswers[i].toLocaleLowerCase()) {
+      grade += 1;
+    }
+  }
 
-  return grade;
+  return grade / correctAnswers.length * 100;
 }
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
+  console.log(`Hello, ${candidateName}. Good luck!`);
   askQuestion();
-  gradeQuiz(this.candidateAnswers);
+  let overallGrade = gradeQuiz(this.candidateAnswers);
+  console.log(
+    `\nCandidate Name: ${candidateName}
+    1) ${questions[0]}
+    Your Answer: ${candidateAnswers[0]}
+    Correct Answer: ${correctAnswers[0]}
+    
+    2) ${questions[1]}
+    Your Answer: ${candidateAnswers[1]}
+    Correct Answer: ${correctAnswers[1]}
+    
+    3) ${questions[2]}
+    Your Answer: ${candidateAnswers[2]}
+    Correct Answer: ${correctAnswers[2]}
+    
+    4) ${questions[3]}
+    Your Answer: ${candidateAnswers[3]}
+    Correct Answer: ${correctAnswers[3]}
+    
+    5) ${questions[4]}
+    Your Answer: ${candidateAnswers[4]}
+    Correct Answer: ${correctAnswers[4]}
+    
+    >>> Overall Grade: ${overallGrade}% (${overallGrade/20} out of 5 responses correct) <<<
+    >>> Status: ${overallGrade >= 80 ? 'PASSED' : 'FAILED'} <<<`
+  )
 }
 
 // Don't write any code below this line //
